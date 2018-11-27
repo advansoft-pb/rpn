a9954d3815647cce35130816e5c14e264a914bf3
advansoft-pb <piotr.borelowski@gmail.com>
2018-11-27 13:43:58 +0100

    fixed an issue

--- src/main/java/pl/advansoft/rpn/RpnCalculator.java
+++ src/main/java/pl/advansoft/rpn/RpnCalculator.java
@@ -1,27 +1,33 @@
 package pl.advansoft.rpn;
 
+import java.util.EmptyStackException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 
 public class RpnCalculator {
+
 	public static int calculate(List<Value> values) throws RpnException {
 		Stack<Value> valueStack = new Stack<>();
-
 		Iterator<Value> iterator = values.iterator();
-		while (iterator.hasNext()) {
-			Value val = iterator.next();
 
-			if (val.isNumber()) {
-				valueStack.push(val);
-			} else if (val.isOperator()) {
-				int b = valueStack.pop().getInt();
-				int a = valueStack.pop().getInt();
-				int x = val.operator().calc(a, b);
-				valueStack.push(new Value(String.valueOf(x)));
+		try {
+			while (iterator.hasNext()) {
+				Value val = iterator.next();
+
+				if (val.isNumber()) {
+					valueStack.push(val);
+				} else if (val.isOperator()) {
+					int b = valueStack.pop().getInt();
+					int a = valueStack.pop().getInt();
+					int x = val.operator().calc(a, b);
+					valueStack.push(new Value(String.valueOf(x)));
+				}
 			}
-		}
 
-		return valueStack.pop().getInt();
+			return valueStack.pop().getInt();
+		} catch (EmptyStackException e) {
+			throw new RpnException(e);
+		}
 	}
 }
